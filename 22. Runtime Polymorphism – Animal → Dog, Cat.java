3c88ec6558c0e5027ade58acb306b20ff54b7295
Jatin Gupta <128694793+jatin009v@users.noreply.github.com>
2026-04-02 07:44:36 +0530

    Create 22. Runtime Polymorphism – Animal → Dog, Cat.java

---
+++ 22. Runtime Polymorphism – Animal → Dog, Cat.java
@@ -0,0 +1,31 @@
+// 22. Show runtime polymorphism with Animal' > 'Dog', Cat' overriding makeSound()'
+
+class Animal {
+    void makeSound() {
+        System.out.println("Animal makes sound");
+    }
+}
+
+class Dog extends Animal {
+    void makeSound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+    void makeSound() {
+        System.out.println("Cat meows");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Animal a;
+
+        a = new Dog();
+        a.makeSound();
+
+        a = new Cat();
+        a.makeSound();
+    }
+}
