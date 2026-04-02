c476d770c8e14396a481f46e0a153ad9714d3cf5
Jatin Gupta <128694793+jatin009v@users.noreply.github.com>
2026-04-02 07:49:31 +0530

    Create 29. Singleton Class (Private Constructor).java

---
+++ 29. Singleton Class (Private Constructor).java
@@ -0,0 +1,23 @@
+// 29. Create a singleton class using private constructor.
+
+class Singleton {
+    private static Singleton instance;
+
+    private Singleton() {
+        System.out.println("Singleton Created");
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+    }
+}
