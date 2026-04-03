93f5ef709f55ed415340d6a7a18a51836be65e73
Jatin Gupta <128694793+jatin009v@users.noreply.github.com>
2026-04-03 22:02:30 +0530

    Create 33_StaticImportDemo.java

---
+++ 33_StaticImportDemo.java
@@ -0,0 +1,8 @@
+import static java.lang.Math.PI;
+
+public class StaticImportDemo {
+    public static void main(String[] args) {
+        // Direct use of PI without Math.PI
+        System.out.println("Value of PI: " + PI);
+    }
+}
