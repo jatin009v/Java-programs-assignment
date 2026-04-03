4e51f42cbb9d871a7614b1d43320df8ace205942
Jatin Gupta <128694793+jatin009v@users.noreply.github.com>
2026-04-03 21:50:12 +0530

    Create 32_SortArray.java

---
+++ 32_SortArray.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class SortArray {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 8, 1, 3};
+        Arrays.sort(arr);
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
+    }
+}
